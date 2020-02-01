115f6da35077bbc34d3f98831ef68169b212e2cc
Hugo Bárzano <hugobarzano@gmail.com>
2020-02-01 18:59:40 +0100

    feat: improve requestAppCreation error: There is another repository with this application name in your github account

--- code-runner/internal/handlers/createApp.go
+++ code-runner/internal/handlers/createApp.go
@@ -6,6 +6,7 @@ import (
 	"code-runner/internal/store"
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 func createApp(w http.ResponseWriter, r *http.Request) {
@@ -18,43 +19,48 @@ func createApp(w http.ResponseWriter, r *http.Request) {
 		}
 	case http.MethodPost:
 
-		ctx:=r.Context()
-		reqApp,err:=getAppFromRequest(r)
+		ctx := r.Context()
+		reqApp, err := getAppFromRequest(r)
 
-		if err!=nil{
+		if err != nil {
 			http.Error(w,
 				fmt.Sprintf("Input Error: %s", err.Error()),
 				http.StatusInternalServerError)
 			return
 		}
 
-		if ok:=reqApp.validateRequest();!ok{
-			_=appsViews["createApp"].Render(w, reqApp)
+		if ok := reqApp.validateRequest(); !ok {
+			_ = appsViews["createApp"].Render(w, reqApp)
 			return
 		}
 
 		session, _ := sessionStore.Get(r, constants.SessionName)
 		user := session.Values[constants.SessionUserName].(string)
 		accessToken := session.Values[constants.SessionUserToken].(string)
-		reqApp.App.Owner=user
+		reqApp.App.Owner = user
 
 		genApp := generator.GenApp{
-			App:reqApp.App,
+			App: reqApp.App,
 		}
 
-		genApp.InitGit(ctx,accessToken)
-		repo,err:=genApp.CreateRepo(ctx)
+		genApp.InitGit(ctx, accessToken)
+		repo, err := genApp.CreateRepo(ctx)
 
-		if err!=nil{
-			http.Error(w,
-				fmt.Sprintf("Git Error: %s", err.Error()),
-				http.StatusInternalServerError)
+		if err != nil {
+			if strings.Contains(err.Error(), "already exists on this account") {
+				reqApp.Errors["Name"] = "There is another repository with this application name in your github account"
+				_ = appsViews["createApp"].Render(w, reqApp)
+			} else {
+				http.Error(w,
+					fmt.Sprintf("Git Error: %s", err.Error()),
+					http.StatusInternalServerError)
+			}
 			return
 		}
 
 		reqApp.App.Repository = repo.GetCloneURL()
-		dao:=store.InitMongoStore(ctx)
-		_,err=dao.CreateApp(ctx,reqApp.App)
+		dao := store.InitMongoStore(ctx)
+		_, err = dao.CreateApp(ctx, reqApp.App)
 		if err != nil {
 			http.Error(w,
 				fmt.Sprintf("DB Error: %s", err.Error()),
@@ -62,11 +68,10 @@ func createApp(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		go genApp.InitializeCode(user,accessToken)
+		go genApp.InitializeCode(user, accessToken)
 
 		http.Redirect(w, r, "/workspace", http.StatusFound)
 	default:
 		fmt.Println("not supported")
 	}
 }
-
