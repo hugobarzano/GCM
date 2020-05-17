21b11cb7847492c20044116c7c16fd93ce4b30a5
Hugo Bárzano <hugobarzano@gmail.com>
2020-05-17 14:11:22 +0200

    ADD makefile

--- code-runner/bin/setup-google.sh
+++ code-runner/bin/setup-google.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 export DOCKER_CERT_PATH="$(pwd)/env/google/remote-daemon/client"
 export DOCKER_HOST="https://127.0.0.1:5555"
-go run main.go --config env/google/gcp-config.json
-
+#go run main.go --config env/google/gcp-config.json
+./code-runner.linux  --config env/google/gcp-config.json
 
 
